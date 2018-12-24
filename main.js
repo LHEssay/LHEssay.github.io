95724324d24c3cf09e513db08188426b7872bed7
Gregory Conrad <gregorysconrad@gmail.com>
2018-12-23 22:19:17 -0500

    price change

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 calculatePriceAndETA = function () {
     const wordCount = parseInt($("#wordCount").val());
     if (wordCount && wordCount > 0) {
-        $("#priceEstimate").text("Estimated price: $" + (wordCount * 0.04).toFixed(2));
+        $("#priceEstimate").text("Estimated price: $" + (wordCount * 0.035).toFixed(2));
 
         $("#dayEstimate").text("Loading day estimate...");
         const api = 'https://spreadsheets.google.com/feeds/cells/';
